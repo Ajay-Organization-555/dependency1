2e0be474e3a8e86df7413303e63c2cd4886093dd
devopsengineerajay589e <devopsengineerajay589@gmail.com>
2025-06-29 17:36:54 +0530

    new explicit rg.tf

--- explicit/rg.tf
+++ explicit/rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "RGEXP" {
   name     = "rgex-1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "RGEXZ" {
+  name     = "rgex-2"
+  location = "West Europe"
+}
